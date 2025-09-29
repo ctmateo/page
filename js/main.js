e91e8af7570340962e14cf137a41fe6fb18c9c4d
Mateo Ramirez <eliermateoramirez@gmail.com>
2025-09-29 13:21:59 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-function traductionDom(language) {
+async function traductionDom(language) {
   const initial_doc = {};
-  console.log('in traduccion')
-  document.querySelectorAll(["translate-text"]).forEach((element) => {
+  console.log("in traduccion");
+
+  // Guardamos los textos originales
+  document.querySelectorAll("[translate-text]").forEach((element) => {
     const id = element.getAttribute("text-id");
     initial_doc[id] = element.innerText;
   });
-  translateAllPage(language)
 
   async function translateText(text, lang) {
     const res = await fetch("https://es.libretranslate.com/translate", {
@@ -17,18 +18,22 @@ function traductionDom(language) {
         format: "text",
         alternatives: 3,
       }),
-      headers: { "Content-Type": application / JSON },
+      headers: { "Content-Type": "application/json" },
     });
+    const data = await res.json();
+    return data.translatedText;
   }
 
-  function translateAllPage(language) {
-    const elements = document.querySelectorAll(["translate-text"]);
-    for (let arg in elements) {
-      const key = arg.getAttribute("text-id");
+  async function translateAllPage(language) {
+    const elements = document.querySelectorAll("[translate-text]");
+    for (let element of elements) {
+      const key = element.getAttribute("text-id");
       const initial_text = initial_doc[key];
-      const dynamic_translate = translateText(initial_text, language);
-      arg.innerText = dynamic_translate
+      const dynamic_translate = await translateText(initial_text, language);
+      element.innerText = dynamic_translate;
     }
   }
-}
 
+  // Ejecutar traducción
+  translateAllPage(language);
+}
